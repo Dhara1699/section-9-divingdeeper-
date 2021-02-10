774a8c07de9ee5d0ce1695814539131140f4b7a4
Dhara <dhara1.patel@bacancy.com>
2021-02-10 11:58:56 +0530

    commit -m"commited!"

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goal></course-goal>
+    <button v-on:click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button v-on:click="setSelectedComponent('manage-goals')">Manage Goals</button>
+   <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>    
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoal from './components/CourseGoal';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue'
 
 export default {
   components: {
      TheHeader,
-     BadgeList,
-     UserInfo,
-     CourseGoal
+     ManageGoals,
+     ActiveGoals,
   },
   data() {
     return {
+      selectedComponent : 'active-goals' ,
+      //selectedComponent2 : 'manage-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods : {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
--- src/components/CourseGoal.vue
+++ src/components/CourseGoal.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
-        <li v-for="goal in goals" :key="goal">{{ goal }}
-            <slot></slot>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-prop="...."></slot>
         </li>
     </ul>
 </template>
